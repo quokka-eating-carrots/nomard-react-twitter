c1583456c8a7c83f628ac6bcd67bda6fd24902d6
quokka-eating-carrots <ovzip7@gmail.com>
2023-02-21 17:22:20 +0900

    2. Authentication

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,21 +1,37 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
+import Navigation from "./Navigation";
+import Profile from "routes/Profile";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
-          <Route exact path="/">
-            <Home />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
         )}
       </Switch>
     </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,20 @@
+import { authService } from "fbase";
+// import { useHistory } from "react-router-dom";
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  // Redirect 방식 말고 useHistory를 사용할 수도 있다.
+  // const history = useHistory()
+  const onLogOutClick = () => {
+    authService.signOut();
+    // history.push("/")
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log-out</button>
+    </>
+  );
 };
 
 export default Profile;
