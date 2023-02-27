b43dff531ba27b48c2bdff6bf9fc8c38e0c396aa
quokka-eating-carrots <ovzip7@gmail.com>
2023-02-27 16:45:24 +0900

    5 Edit Profile

--- src/components/App.js
+++ src/components/App.js
@@ -8,16 +8,35 @@ function App() {
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setUserObj(user);
+        // setUserObj(user);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
       }
       setInit(true);
     });
   }, []);
 
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    // 이런 방법도 있지만 에러가 자주 발생한다
+    // setUserObj(Object.assign({}, user));
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  };
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+        <AppRouter
+          isLoggedIn={Boolean(userObj)}
+          userObj={userObj}
+          refreshUser={refreshUser}
+        />
       ) : (
         "Initailzing..."
       )}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,8 +1,10 @@
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
 // import { useHistory } from "react-router-dom";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Profile = () => {
+const Profile = ({ userObj, refreshUser }) => {
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
   // Redirect 방식 말고 useHistory를 사용할 수도 있다.
   // const history = useHistory()
   const onLogOutClick = () => {
@@ -10,8 +12,41 @@ const Profile = () => {
     // history.push("/")
   };
 
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+      refreshUser();
+    }
+  };
+
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+    console.log(nweets.docs.map((doc) => doc.data()));
+    // =>은 return이다??!
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="Display name" onChange={onChange} />
+        <input type="submit" value="update profile" />
+      </form>
       <button onClick={onLogOutClick}>Log-out</button>
     </>
   );
